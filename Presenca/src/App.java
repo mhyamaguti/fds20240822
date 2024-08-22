7d5c8fc8290cdcc42ee2648f6c30d970806a206d
Marcelo H. Yamaguti <yamaguti@pucrs.br>
2024-08-22 17:36:12 +0000

    Primeiro commit

---
+++ Presenca/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Turma 32 de Fundamentos de Desenvolvimento de Software");
+        System.out.println("Presentes:");
+        // Insira abaixo uma nova linha que mostra na tela o seu nome completo
+    }
+}
