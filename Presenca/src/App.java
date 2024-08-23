c864e13888769d1ced644b305c88e24325dd5383
gabriel.bueno <gabriel.bueno@zallpy.com>
2024-08-22 22:03:48 -0300

    Gabriel Bueno

--- Presenca/src/App.java
+++ Presenca/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Turma 32 de Fundamentos de Desenvolvimento de Software");
         System.out.println("Presentes:");
+        System.out.println("Gabriel da Silva Bueno");
         // Insira abaixo uma nova linha que mostra na tela o seu nome completo
         System.out.println("MHY");
     }
