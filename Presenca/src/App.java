4005bbd2869f15b07ed1d1433eb6e65d286e63b7
Estevam Cabral <58441670+estevamcabral@users.noreply.github.com>
2024-08-23 01:06:57 +0000

    Estevam Cabral Pacheco

--- Presenca/src/App.java
+++ Presenca/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Turma 32 de Fundamentos de Desenvolvimento de Software");
         System.out.println("Presentes:");
+        System.out.println("Estevam Cabral Pacheco");
         // Insira abaixo uma nova linha que mostra na tela o seu nome completo
         System.out.println("MHY");
     }
--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # fds20240822
 Presenca da aula do dia 22/8/2024 de FDS
-
-Estevam Cabral Pacheco
