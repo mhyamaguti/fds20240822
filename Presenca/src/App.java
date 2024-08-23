4c0c549a7e2055faf382392610a2d21b0991c8dc
Isadora Camargo <143833334+isadoracamarg@users.noreply.github.com>
2024-08-23 01:08:16 +0000

    Isadora Morari - Presença

--- Presenca/src/App.java
+++ Presenca/src/App.java
@@ -3,6 +3,6 @@ public class App {
         System.out.println("Turma 32 de Fundamentos de Desenvolvimento de Software");
         System.out.println("Presentes:");
         // Insira abaixo uma nova linha que mostra na tela o seu nome completo
-        System.out.println("MHY");
+        System.out.println("Isadora Morari");
     }
 }
