c22fc816f5719e1b4a3567f67748eb4fb12f30ad
sbcSpohr <114929637+sbcSpohr@users.noreply.github.com>
2024-08-23 01:22:47 +0000

    Breno Spohr

--- Presenca/src/App.java
+++ Presenca/src/App.java
@@ -3,6 +3,7 @@ public class App {
         System.out.println("Turma 32 de Fundamentos de Desenvolvimento de Software");
         System.out.println("Presentes:");
         // Insira abaixo uma nova linha que mostra na tela o seu nome completo
+        System.out.println("Breno Spohr Bernardi da Cunha");
         System.out.println("MHY");
     }
 }
